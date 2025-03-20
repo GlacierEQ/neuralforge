c94a1360398b9441a443ed42ac0970b097611d0e
FabianVF26 <fvargasf@ucenfotec.ac.cr>
2025-03-19 21:15:57 -0600

    Validacion Retablacer Contrasena con Anotaciones

--- neuralforge_api/src/main/java/com/cenfotec/p3/neuralforge_api/model/resource/PasswordResetRequestResource.java
+++ neuralforge_api/src/main/java/com/cenfotec/p3/neuralforge_api/model/resource/PasswordResetRequestResource.java
@@ -1,5 +1,7 @@
 package com.cenfotec.p3.neuralforge_api.model.resource;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
@@ -9,6 +11,12 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class PasswordResetRequestResource {
 
+
+    @NotBlank(message = "Email cannot be empty")
+    @Pattern(
+            regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$",
+            message = "Email should be a valid email format"
+    )
     private String email;
 
 }
