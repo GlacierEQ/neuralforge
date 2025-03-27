813e6935a59e15442158bd67e2020c55daa6371d
Enrique Alpízar <ealpizr@gmail.com>
2025-03-26 18:14:21 -0600

    feat(frontend): refactor create project dialog to use reusable project form component

--- neuralforge_ui/src/app/components/dialogs/create-project-dialog/create-project-dialog.component.ts
+++ neuralforge_ui/src/app/components/dialogs/create-project-dialog/create-project-dialog.component.ts
@@ -1,50 +1,32 @@
 import { CommonModule } from "@angular/common";
 import { Component, inject } from "@angular/core";
-import { FormsModule } from "@angular/forms";
-import { MatButtonModule } from "@angular/material/button";
 import { MatDialogModule, MatDialogRef } from "@angular/material/dialog";
-import { MatFormFieldModule } from "@angular/material/form-field";
-import { MatIconModule } from "@angular/material/icon";
-import { MatInputModule } from "@angular/material/input";
 import { AlertService } from "../../../services/alert.service";
 import { LearningProjectService } from "../../../services/learning-project.service";
+import {
+  ProjectFormComponent,
+  ProjectFormData,
+} from "../../project-form/project-form.component";
 
 @Component({
   selector: "app-create-project-dialog",
   standalone: true,
   templateUrl: "./create-project-dialog.component.html",
   styleUrls: ["./create-project-dialog.component.scss"],
-  imports: [
-    CommonModule,
-    MatDialogModule,
-    MatButtonModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatIconModule,
-    FormsModule,
-  ],
+  imports: [CommonModule, MatDialogModule, ProjectFormComponent],
 })
 export class CreateProjectDialogComponent {
   private learningProjectService = inject(LearningProjectService);
   private alertService = inject(AlertService);
 
-  project = {
+  project: ProjectFormData = {
     name: "",
     description: "",
   };
 
   constructor(private dialogRef: MatDialogRef<CreateProjectDialogComponent>) {}
 
-  onSubmit() {
-    if (!this.project.name.trim()) {
-      return;
-    }
-
-    const projectData = {
-      name: this.project.name,
-      description: this.project.description || null,
-    };
-
+  onSubmit(projectData: ProjectFormData) {
     this.learningProjectService.add(projectData).subscribe({
       next: (response: any) => {
         this.alertService.displayAlert(
